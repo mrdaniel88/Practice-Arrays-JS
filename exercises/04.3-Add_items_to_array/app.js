dba33bc92c058b480970a0b706d5fa933b73cf74
Daniel Martinez <d.martinez74@gmail.com>
2023-03-13 15:01:18 +0000

    Corrections made on some excercises

--- exercises/04.3-Add_items_to_array/app.js
+++ exercises/04.3-Add_items_to_array/app.js
@@ -2,6 +2,6 @@ let arr = [4,5,734,43,45];
 
 // Your code here, use the push function and the random function.
 for (let i = 0; i < 2; i++){
-    arr.push(Math.floor(Math.random()))
+    arr.push(Math.floor(Math.random() * 100))
 }
 console.log(arr);
--- exercises/04.4-Add_items_to_array_looping/app.js
+++ exercises/04.4-Add_items_to_array_looping/app.js
@@ -5,6 +5,6 @@ let arr = [4,5,734,43,45];
 // you need to loop 10 times, for example, using a for loop
 //*****************
 for (let i = 0; i < 10; i++) {
-    arr.push(Math.floor(Math.random()))
+    arr.push(Math.floor(Math.random() * 100))
 }
 console.log(arr)
