0ab6ddf4df3455fa43051d49bfdaeee65ae0c1b6
Daniel Martinez <d.martinez74@gmail.com>
2023-03-15 22:09:35 +0000

    Done until 24, corrections needed for 25

--- exercises/24-Making-a-ul/app.js
+++ exercises/24-Making-a-ul/app.js
@@ -1,33 +1,56 @@
-let allColors = [
-	{label: 'Red', sexy: true},
-	{label: 'Pink', sexy: false},
-	{label: 'Orange', sexy: true},
-	{label: 'Brown', sexy: false},
-	{label: 'Pink', sexy: true},
-	{label: 'Violet', sexy: true},
-	{label: 'Purple', sexy: false},
+let allColors = [{
+        label: 'Red',
+        sexy: true
+    },
+    {
+        label: 'Pink',
+        sexy: false
+    },
+    {
+        label: 'Orange',
+        sexy: true
+    },
+    {
+        label: 'Brown',
+        sexy: false
+    },
+    {
+        label: 'Pink',
+        sexy: true
+    },
+    {
+        label: 'Violet',
+        sexy: true
+    },
+    {
+        label: 'Purple',
+        sexy: false
+    },
 ];
 
-function generateLI(color){
-	// your code here
+function generateLI(color) {
+    // your code here
+    return `<li>${color.label}</li>`
 }
 
-function filterColors(color){
-	// your code here
+function filterColors(color) {
+    // your code here
+    if (color.sexy == true) {
+        return color;
+    }
 }
 
-function generateHTMLFromArray(array){
-	
-	let filteredOptions = array.filter(filterColors);
-	let LIs = filteredOptions.map(generateLI);
+function generateHTMLFromArray(array) {
 
-	let htmlString = '<ul>';
-	LIs.forEach(function(elm){
-		htmlString += elm;
-	})
-	htmlString += '</ul>';
-	return htmlString;
+    let filteredOptions = array.filter(filterColors);
+    let LIs = filteredOptions.map(generateLI);
+
+    let htmlString = '<ul>';
+    LIs.forEach(function(elm) {
+        htmlString += elm;
+    })
+    htmlString += '</ul>';
+    return htmlString;
 }
 
 console.log(generateHTMLFromArray(allColors));
-
--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,9 +1,19 @@
 // Add your code here
+function lyricsGenerator(lyric) {
+    let transformation = lyric.map(number => {
+        if (number == 0) {
+            number = 'Boom';
+        } else if (number == 1) {
+            number = 'Drop the base';
+        }
+    });
+    return transformation;
+}
 
 
 // Don't change anything bellow this line
-console.log(lyricsGenerator([0,0,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,0]))
-console.log(lyricsGenerator([1,0,1]))
-console.log(lyricsGenerator([1,1,1]))
+console.log(lyricsGenerator([0, 0, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 1, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 0]))
+console.log(lyricsGenerator([1, 0, 1]))
+console.log(lyricsGenerator([1, 1, 1]))
