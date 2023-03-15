e6f284d72d4044a69103c33f3a41075b3db22843
Daniel Martinez <d.martinez74@gmail.com>
2023-03-15 20:07:27 +0000

    Done until excercise 20.5 (know age by birthdate)

--- exercises/20.5-Map-arrays-of-objects/app.js
+++ exercises/20.5-Map-arrays-of-objects/app.js
@@ -1,13 +1,31 @@
-let people = [
-	{ name: 'Joe', birthDate: new Date(1986,10,24) },
-	{ name: 'Bob', birthDate: new Date(1975,5,24) },
-	{ name: 'Erika', birthDate: new Date(1989,6,12) },
-	{ name: 'Dylan', birthDate: new Date(1999,12,14) },
-	{ name: 'Steve', birthDate: new Date(2003,4,24) }
+let people = [{
+        name: 'Joe',
+        birthDate: new Date(1986, 10, 24)
+    },
+    {
+        name: 'Bob',
+        birthDate: new Date(1975, 5, 24)
+    },
+    {
+        name: 'Erika',
+        birthDate: new Date(1989, 6, 12)
+    },
+    {
+        name: 'Dylan',
+        birthDate: new Date(1999, 12, 14)
+    },
+    {
+        name: 'Steve',
+        birthDate: new Date(2003, 4, 24)
+    }
 ];
 
-let simplifier = function(person){
-	return person.name;
+let simplifier = function(person) {
+    const today = new Date();
+    const age = today.getFullYear() - person.birthDate.getFullYear() -
+        (today.getMonth() < person.birthDate.getMonth() ||
+            (today.getMonth() === person.birthDate.getMonth() && today.getDate() < person.birthDate.getDate()))
+    return `Hello, my name is ${person.name} and I am ${age} years old`;
 };
 
 console.log(people.map(simplifier));
